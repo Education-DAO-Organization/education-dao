ae1384e14d8e1baa3dd54e9539b02eaf57506184
viphan007 <vipt@younetgroup.com>
2023-11-07 12:01:53 +0700

    Integrate precommit-hook

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,47 +1,54 @@
-"use client"
-import React, { useEffect, useRef, useState } from 'react';
-import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
-import Image from 'next/image'
-import clsx from 'clsx';
-import styles from './navBar.module.scss'
-import Link from 'next/link'
-import Logo from '@/assets/icons/logo.svg';
-import DiscordLogo from '@/assets/icons/discord-logo.svg';
-import TwitterLogo from '@/assets/icons/twitter-logo.svg';
-import MetaMaskLogo from '@/assets/icons/metamask-fox-logo.svg';
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import Image from "next/image";
+import clsx from "clsx";
+import styles from "./navBar.module.scss";
+import Link from "next/link";
+import Logo from "@/assets/icons/logo.svg";
+import DiscordLogo from "@/assets/icons/discord-logo.svg";
+import TwitterLogo from "@/assets/icons/twitter-logo.svg";
+import MetaMaskLogo from "@/assets/icons/metamask-fox-logo.svg";
 
 type MenuItemProps = {
   items: {
-    title: string,
-    url: string,
-    newTab: boolean,
-    icon?: string,
-  }[]
-}
+    title: string;
+    url: string;
+    newTab: boolean;
+    icon?: string;
+  }[];
+};
 
 function Menu({ items }: MenuItemProps) {
   return (
     <>
       <ul className={styles.menuList}>
-        {
-          items.map((item, index) => <li key={index}>
+        {items.map((item, index) => (
+          <li key={index}>
             <a href={item.url} target={item.newTab ? "_blank" : "_self"}>
-              {item.icon && <Image src={item.icon} alt={item.title} width={24} height={24} />}
+              {item.icon && (
+                <Image
+                  src={item.icon}
+                  alt={item.title}
+                  width={24}
+                  height={24}
+                />
+              )}
               {item.title}
             </a>
-          </li>)
-        }
+          </li>
+        ))}
       </ul>
       <div className={styles.connectWallet}>
         <Image src={MetaMaskLogo} alt="MetaMask Fox" width={24} height={22} />
         <span>Connect with MetaMask</span>
       </div>
     </>
-  )
+  );
 }
 
 function NavBar() {
-  const overlayMenuRef = useRef(null)
+  const overlayMenuRef = useRef(null);
   const [toggle, toggleNav] = useState(false);
 
   useEffect(() => {
@@ -50,9 +57,9 @@ function NavBar() {
     if (toggle) {
       disableBodyScroll(overlayMenuRef.current);
     } else {
-      enableBodyScroll(overlayMenuRef.current)
+      enableBodyScroll(overlayMenuRef.current);
     }
-  }, [toggle])
+  }, [toggle]);
 
   const navItems = [
     {
@@ -71,8 +78,8 @@ function NavBar() {
       url: "#",
       newTab: true,
       icon: TwitterLogo,
-    }
-  ]
+    },
+  ];
   return (
     <div>
       <div className={styles.headerPlaceholder} />
@@ -81,25 +88,36 @@ function NavBar() {
           <nav className={styles.innerWrapper}>
             <div className={styles.imageWrapper}>
               <Link href="/">
-                <Image src={Logo} alt="Education DAO logo" width={64} height={64} />
+                <Image
+                  src={Logo}
+                  alt="Education DAO logo"
+                  width={64}
+                  height={64}
+                />
               </Link>
             </div>
             <div className={styles.right}>
               <Menu items={navItems} />
             </div>
-            <button className={clsx(styles.toggleIcon, { [styles.open]: toggle })} onClick={() => toggleNav(!toggle)}>
+            <button
+              className={clsx(styles.toggleIcon, { [styles.open]: toggle })}
+              onClick={() => toggleNav(!toggle)}
+            >
               <div />
               <div />
               <div />
             </button>
           </nav>
         </div>
-        <div ref={overlayMenuRef} className={clsx(styles.menuOverlay, { [styles.open]: toggle })}>
+        <div
+          ref={overlayMenuRef}
+          className={clsx(styles.menuOverlay, { [styles.open]: toggle })}
+        >
           <Menu items={navItems} />
         </div>
       </header>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
