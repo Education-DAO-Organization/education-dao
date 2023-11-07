c689341bce207e0e25c4d7dd1af4950d84fe39c9
viphan007 <vipt@younetgroup.com>
2023-11-07 11:11:24 +0700

    Update basePath on next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/education-dao',
   sassOptions: {
     prependData: `@import "src/styles/functions.scss";`,
   }
