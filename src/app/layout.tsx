c1f2d12bc3b636eb2a8d94a3b16c21b90bd30efd
jlazoff <13966291+jlazoff@users.noreply.github.com>
2023-12-01 14:07:52 -0500

    Add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from 'next/head';
 import fraktionSansFont from "@/fonts/fraktionSans";
 import styles from "./layout.module.scss";
 import "@/styles/globals.scss";
@@ -15,7 +16,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-567GV6F');
+          `
+        }} />
+      </Head>
       <body className={fraktionSansFont.className}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-567GV6F" height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
+        </noscript>
         <main className={styles.main}>{children}</main>
       </body>
     </html>
