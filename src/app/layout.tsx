b78d28cc5a2abea34c720e69ae9bcd88e1a4da20
jlazoff <13966291+jlazoff@users.noreply.github.com>
2023-12-01 14:41:48 -0500

    GTM Hardcoded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { GoogleTagManager } from 'react-gtm-module';
+import Head from "next/head";
 import fraktionSansFont from "@/fonts/fraktionSans";
 import styles from "./layout.module.scss";
 import "@/styles/globals.scss";
@@ -16,9 +16,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-567GV6F');
+          `,
+          }}
+        />
+      </Head>
       <body className={fraktionSansFont.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-567GV6F"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <main className={styles.main}>{children}</main>
-        <GoogleTagManager gtmId="GTM-567GV6F" />
       </body>
     </html>
   );
